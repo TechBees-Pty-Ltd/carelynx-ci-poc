b92aa9ccf88571a43cde478e9e40511bc313ebd5
Lintonio <linton@carelynx.com>
2024-07-30 20:06:50 +1000

    Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Sidebar from './views/Sidebar'
 
 function App() {
 
   return (
     <>
+      <Sidebar />
       <h1 className="text-4xl font-bold">Landing Page for CI</h1>
     </>
   )
